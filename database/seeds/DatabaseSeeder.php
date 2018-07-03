9c73c12176ae31328a50df0ed4da540d1b107d45
hai nguyen <hainguyen4391@gmail.com>
2018-07-03 09:46:45 +0700

    edit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+    	DB::table('users')->insert([
+    		'username' => 'admin',
+    		'email' => 'admin@gmail.com',
+    		'password' => '123',
+    		'fullname' => 'Admin',
+    		'phone' => '123456',
+    		'address' => '123 da nang',
+    		'password' => bcrypt('123'),
+    		'level' => 1,
+    		'avatar' => '123avatar',
+    		'active' => 1,
+    	]);
     }
 }
