ee9a8eec10cd4e1000ceb3d63a19ed69b599e31f
hai nguyen <hainguyen4391@gmail.com>
2018-07-03 09:09:29 +0700

    edit

--- resources/views/user/layout/page.blade.php
+++ resources/views/user/layout/page.blade.php
@@ -5,6 +5,7 @@
 	<title>Bootstrap E-commerce Templates</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 	<base href="{{asset('')}}">
 	<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
 	<!-- bootstrap -->
