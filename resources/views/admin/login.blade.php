fdea55f70b69b291a42d0477e6fbec1a662d28fd
hai nguyen <hainguyen4391@gmail.com>
2018-07-03 16:09:39 +0700

    edit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Khóa Học Lập Trình Laravel Framework 5.x Tại Khoa Phạm">
     <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <title>Admin - Khoa Phạm</title>
     <base href="{{asset('')}}">
