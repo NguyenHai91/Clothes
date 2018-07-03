87212bd5e1c4146b59921843b6b9689851172419
hai nguyen <hainguyen4391@gmail.com>
2018-07-03 18:03:58 +0700

    edit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -119,7 +119,7 @@ class IndexController extends Controller
 		$productDetail = ProductDetail::where('product_id',$id)->where('size_id',$request->slcSize)->where('color_id',$request->slcColor)->get()->first();
 		$productBuy = Product::findOrFail($id);
 
-		Cart::add(['id'=>$productDetail['id'], 'name'=>$productBuy['name'],'qty'=>$request['txtQuant'],'price'=>$productBuy['price'],'options'=>['image'=>$productBuy['image'], 'size' => $size['size'], 'color'=>$color['name']]]);
+		Cart::add(['id'=>$productDetail->id, 'name'=>$productBuy['name'],'qty'=>$request['txtQuant'],'price'=>$productBuy['price'],'options'=>['image'=>$productBuy['image'], 'size' => $size['size'], 'color'=>$color['name']]]);
 		$productInCart = Cart::content();
 		return redirect('cart');
 	}
