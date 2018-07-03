84efc294c3491840967c44d978b42b18ac857b76
hai nguyen <hainguyen4391@gmail.com>
2018-07-03 15:42:55 +0700

    edit

--- config/session.php
+++ config/session.php
@@ -135,7 +135,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'webshopping.herokuapp.com'),
+    'domain' => env('COOKIE_DOMAIN', 'webshopping.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
